fe0016fa63c2d3d11f7cadf4c990cfb66feec689
Dominik Heidler <dheidler@suse.de>
2020-08-05 23:42:21 +0200

    Implement search for callsign or MySQL filter expr

--- sqlog/web.py
+++ sqlog/web.py
@@ -4,9 +4,8 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 
-import argparse
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
 
 import sqlog
 import sqlog.qso
@@ -16,14 +15,31 @@ sqlog.Config.load(os.environ.get('CONFIG', 'config.cfg'))
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def logbook():
 	connection = sqlog.mysql_connection()
 	try:
 		with connection.cursor() as cursor:
-			cursor.execute('SELECT * FROM qsos')
+			s = request.args.get('s', '')
+			"""
+			if not s:
+				query = ''
+			elif ' ' in s or '=' in s:
+				query = 'WHERE %s' % s
+			elif '/' in query:
+				query = 'WHERE callsign = "%s"' % s #FIXME: escapt
+			else:
+			"""
+			if not s:
+				query = ''
+			elif not (' ' in s or '=' in s):
+				s = s.upper()
+				c = pymysql.escape_string(s)
+				query = 'WHERE callsign = "%s" OR callsign LIKE "%%/%s/%%" OR callsign LIKE "%%/%s" OR callsign LIKE "%s/%%"' % (c,c,c,c)
+			cursor.execute('SELECT * FROM qsos %s ORDER BY datetime' % query)
 			qsos = map(sqlog.qso.QSO, cursor.fetchall())
-			return render_template('logbook.html', qsos=qsos)
+			return render_template('logbook.html', qsos=qsos, s=s)
 	finally:
 		connection.close()
 
